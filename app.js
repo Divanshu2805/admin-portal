6f1c5e67b220d87fefef9db08151f4d94db5dad6
Divanshurajgupta <Divanshurajgupta@gmail.com>
2025-07-09 01:56:12 +0530

    update db

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
 // MongoDB Connect
-mongoose.connect('mongodb://127.0.0.1:27017/adminPortalDB', {
+mongoose.connect('process.env.MONGO_URI', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("✅ MongoDB Connected"))

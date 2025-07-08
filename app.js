d3a99da18850b0cd6f5b415a443daf11b2134ba4
Divanshurajgupta <Divanshurajgupta@gmail.com>
2025-07-09 02:08:14 +0530

    remove string

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 
 // MongoDB Connect
-mongoose.connect('process.env.MONGO_URI', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("✅ MongoDB Connected"))
